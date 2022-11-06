cab193c4611e45e7dbfb82edfa970ce79a0ae95c
Sc1mitarDEV <117548651+Sc1mitarDEV@users.noreply.github.com>
2022-11-06 14:45:01 +0800

    Create DarkHallowX.lua

---
+++ DarkHallowX.lua
@@ -0,0 +1,10 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "DarkHallowX | DOORS 👁️", HidePremium = false, SaveConfig = true, ConfigFolder = "DarkHallowCF"})
+
+
+
+
+
+
+end
+OrionLib:Destroy()
