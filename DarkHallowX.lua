19a2624c7803719e86fe663112acabb312aa2307
Sc1mitarDEV <117548651+Sc1mitarDEV@users.noreply.github.com>
2022-11-06 15:09:06 +0800

    Update DarkHallowX.lua

--- DarkHallowX.lua
+++ DarkHallowX.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 6516141723 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "DarkHallowX | DOORS 👁️", HidePremium = false, SaveConfig = true, ConfigFolder = "DarkHallowCF"})
+local Window = OrionLib:MakeWindow({Name = "DarkHallowX | DOORS 👁️", HidePremium = false, IntroEnabled = false,IntroText = "Doors Supported!",SaveConfig = true, ConfigFolder = "DarkHallowCF"})
 
 local PrivateTab = Window:MakeTab({
 	Name = "(Private features)",
